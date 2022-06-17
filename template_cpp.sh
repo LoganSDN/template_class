7252444a512f28b6a45b98e54615fab883290dca
Logan SIDAN <logan.sidan@icloud.com>
2022-06-17 18:11:03 +0200

    New feature : multiple args

--- template_cpp.sh
+++ template_cpp.sh
@@ -1,10 +1,13 @@
-#!/bin/zsh
+#!/bin/bash
 git clone --quiet "https://github.com/LoganSDN/template_class.git" template
 g++ template/sed.cpp -o template/sed
-./template/sed template/template.cpp "€" $1
-./template/sed template/template.hpp "€" $1
 mkdir -p class
 mkdir -p class_def
-mv template/template.cpp.replace class_def/$1.cpp
-mv template/template.hpp.replace class/$1.hpp
+for (( i=1; i<=$#; i++)); 
+do
+	./template/sed template/template.cpp "€" ${!i}
+	./template/sed template/template.hpp "€" ${!i}
+	mv template/template.cpp.replace class_def/${!i}.cpp
+	mv template/template.hpp.replace class/${!i}.hpp
+done
 rm -rf template
